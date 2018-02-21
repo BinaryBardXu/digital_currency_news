5f8a17ab40320d2b4a06f798f872c997cbf26f83
Tao <bitao.ux@gmail.com>
2018-02-21 22:51:00 +0800

    Optimize project structure and fix a error.

--- app/api/api_1_0/__init__.py
+++
@@ -1,4 +0,0 @@
-from flask import Blueprint
-from . import news
-
-api = Blueprint('api_1_0', __name__)
--- app/apis/api_1_0/news.py
+++ app/apis/api_1_0/news.py
@@ -1,15 +1,15 @@
 from app.services.news_service import get_latest_news
 from flask import jsonify
 
-from . import api
+from . import api_1_0
 
 
-@api.route('/')
+@api_1_0.route('/')
 def hello_world():
     return 'Hello, World!'
 
 
-@api.route('/news', methods=['GET'])
+@api_1_0.route('/news', methods=['GET'])
 def get():
     news_list = get_latest_news()
     print(news_list)
