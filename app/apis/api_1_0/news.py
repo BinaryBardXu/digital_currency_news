eb7a310e2324722cb03b48298fe5f53702f946fd
Tao <bitao.ux@gmail.com>
2018-03-05 11:07:07 +0800

    Integrating restful and blueprint.

--- app/apis/api_1_0/news.py
+++ app/apis/api_1_0/news.py
@@ -1,16 +1,15 @@
 from app.services.news_service import get_latest_news
-from flask import jsonify
+from flask_restful import Api, Resource
 
 from . import api_1_0
 
-
-@api_1_0.route('/')
-def hello_world():
-    return 'Hello, World!'
+api = Api(api_1_0)
 
 
-@api_1_0.route('/news', methods=['GET'])
-def get():
-    news_list = get_latest_news()
-    print(news_list)
-    return jsonify(news_list)
+class News(Resource):
+    def get(self):
+        news_list = get_latest_news()
+        return news_list
+
+
+api.add_resource(News, '/news')
